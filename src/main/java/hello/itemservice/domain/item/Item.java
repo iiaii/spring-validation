d98d684526c3490b96e3e681cdefe87d93bc1d72
iiaii <jhun9409@naver.com>
2021-10-22 23:34:20 +0900

    feat: Bean validation groups(실무에서는 사용x)

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -16,17 +16,18 @@ import java.util.Objects;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10_000", message = "최소 총합은 10,000 입니다")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message = "공백 X")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1_000, max = 1_000_000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1_000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9_999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9_999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
